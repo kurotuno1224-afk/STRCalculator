f43e9ec0ae34039a784162473c36fb709a8eebd0
kurotuno-afk <kurotuno1224@gmail.com>
2025-12-17 22:04:56 +0900

    can test hsr-assets

--- client/main.tsx
+++ client/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import HomeRoute from "./routes/index";
 import RootLayout from "./routes/root";
 import "./styles/global.css";
+import HsrAssetsRoute from "./routes/hsr-assets";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<HomeRoute />} />
+          <Route path="hsr-asssete" element={<HsrAssetsRoute />} />
         </Route>
       </Routes>
     </BrowserRouter>
